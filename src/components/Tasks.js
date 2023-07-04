cf689bbc236c8954884578576f817226308cb59c
sudoarun <arunrajorya1999@gmail.com>
2023-07-04 15:13:54 +0530

    Tasks Component done

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,31 @@
-import React from "react";
+import { onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import ref from "../service/collectionRef";
 
 const Tasks = () => {
+  const [tasklist, setTaskList] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(ref, (snapshot) => {
+      setTaskList(
+        snapshot.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }))
+      );
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  console.log(tasklist);
   return (
     <div>
       <h5>Tasks Will SHow Here</h5>
+      <div>
+        {tasklist.map(({ id, data }) => (
+          <li key={id}>{data.todo}</li>
+        ))}
+      </div>
     </div>
   );
 };
